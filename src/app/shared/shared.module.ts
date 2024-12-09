4f1c444f47cf265f1b0921cff09da8a82d3d71c4
GabrielRQuinteros <QuinterosGabrielRoberto@hotmail.com>
2024-12-08 22:22:45 -0300

    Agregado-Primeng-PrimeIcons-PrimeFlex

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MenuComponent } from './components/menu/menu.component';
+import { PrimeNgModule } from '../prime-ng/prime-ng.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MenubarModule } from 'primeng/menubar';
+
+
+
+@NgModule({
+  declarations: [
+    MenuComponent
+  ],
+  imports: [
+    CommonModule,
+    PrimeNgModule,
+    BrowserAnimationsModule,
+  ],
+  exports: [
+    MenuComponent
+  ]
+})
+export class SharedModule { }
